4beb7c3a8a00c43b309ebd719f0a5630055d888d
Marc Retzlaff <retzlaff@hs-pforzheim.de>
2019-12-24 13:14:52 +0100

    Added Kreis

--- main.cpp
+++ main.cpp
@@ -3,13 +3,15 @@ using namespace std;
 
 #include "Punkt.h"
 #include "Linie.h"
+#include "Kreis.h"
 
 int main()
 {
-/* Samples */
+/* Kreis */
 Punkt m(2.0,2.0);
+Kreis sK(m,3.0);
 Punkt punkt1(3.0,4.0);
-Linie linie1(m.punkt1);
+Linie linie1(m,punkt1);
 cout << "Hello World" << endl;
 return 0;
 }

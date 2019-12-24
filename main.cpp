31767134fbbae2efd799ba06bb82b6c71143be8e
Marc Retzlaff <retzlaff@hs-pforzheim.de>
2019-12-24 13:10:47 +0100

    Added Linie to project

--- main.cpp
+++ main.cpp
@@ -2,11 +2,14 @@
 using namespace std;
 
 #include "Punkt.h"
+#include "Linie.h"
 
 int main()
 {
+/* Samples */
 Punkt m(2.0,2.0);
 Punkt punkt1(3.0,4.0);
-cout << "Hello World!" << endl;
+Linie linie1(m.punkt1);
+cout << "Hello World" << endl;
 return 0;
 }
